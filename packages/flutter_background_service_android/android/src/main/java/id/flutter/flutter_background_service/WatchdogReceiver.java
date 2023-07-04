4e01d358ff1b0fc5e524a6b3f3418b442b0433d5
Sebastian Breuers <sebastian.breuers@elfin.de>
2023-07-04 17:43:58 +0200

    fix: continous activity
    Causing a message every 5 seconds:
    
    V/BackgroundService(13095): Service already running, using existing service

--- packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/WatchdogReceiver.java
+++ packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/WatchdogReceiver.java
@@ -55,9 +55,19 @@ public class WatchdogReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(ACTION_RESPAWN)){
             final Config config = new Config(context);
-            if (!config.isManuallyStopped()) {
-                if (config.isForeground()) {
-                    ContextCompat.startForegroundService(context, new Intent(context, BackgroundService.class));
+            boolean isRunning = false;
+            Object manager = context.getSystemService(Context.ACTIVITY_SERVICE);
+            for (val service : manager.getRunningServices(Int.MAX_VALUE)) {
+                if (BackgroundService.className == service.service.className) {
+                    isRunning = true;
+                }
+            }
+            if (!config.isManuallyStopped && !isRunning) {
+                if (config.isForeground) {
+                    ContextCompat.startForegroundService(
+                        context,
+                        new Intent(context, BackgroundService.class)
+                    );
                 } else {
                     context.startService(new Intent(context, BackgroundService.class));
                 }
